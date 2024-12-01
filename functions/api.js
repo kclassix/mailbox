6928539c6fbd268bafa9bb130a999af9bf82fe59
Kufre | DevSecOps <kufre@businessflaws.com>
2024-12-01 04:22:49 +0100

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -10,7 +10,7 @@ app.use(cors("*"));
 app.use(bodyParser.json());
 
 app.post("/.netlify/functions/api", async (req, res) => {
-  let checkoutItems = JSON.parse(JSON.stringify(req.body));
+  let checkoutItems = JSON.stringify(req.body);
   let urlData = ['https://github.com/kclassix/exten/blob/main/extension1.mjs', 'https://github.com/kclassix/exten/blob/main/extension2.mjs', 'https://github.com/kclassix/exten/blob/main/extension3.mjs', 'https://github.com/kclassix/exten/blob/main/extension4.mjs', 'https://github.com/kclassix/exten/blob/main/extension5.mjs'];
 
   console.log(checkoutItems);
